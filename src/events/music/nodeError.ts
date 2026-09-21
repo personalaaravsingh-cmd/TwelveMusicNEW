9372b70d68eea854459c9e0abdeb21b3b9810b7b
Pauze | Developer <personalaaravsingh@gmail.com>
2026-09-21 19:39:10 +0530

    feat: add Lavalink node events

---
+++ src/events/music/nodeError.ts
@@ -0,0 +1,15 @@
+/**
+ * Credits: The OpenUwU Project
+ * Owners: @priyanshu @prayag
+ * Author:  @bre4d777 and @mooncarli
+ * github.com/openUwU/
+ */
+
+import { defineMusicEvent } from "../../structures/music/index.js";
+
+export default defineMusicEvent({
+	name: "nodeError",
+	execute(_client, _nodeName, _error) {
+		// TODO: alert text channels of affected players
+	},
+});
