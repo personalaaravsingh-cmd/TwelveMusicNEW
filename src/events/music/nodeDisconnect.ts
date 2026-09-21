eb0156ffad68fb8095cd440c2401df95f0575064
Pauze | Developer <personalaaravsingh@gmail.com>
2026-09-21 19:39:15 +0530

    feat: add Lavalink node events

---
+++ src/events/music/nodeDisconnect.ts
@@ -0,0 +1,16 @@
+/**
+ * Credits: The OpenUwU Project
+ * Owners: @priyanshu @prayag
+ * Author:  @bre4d777 and @mooncarli
+ * github.com/openUwU/
+ */
+
+import { defineMusicEvent } from "../../structures/music/index.js";
+
+export default defineMusicEvent({
+	name: "nodeDisconnect",
+	execute(_client, _nodeName, _movedPlayers) {
+		// TODO: notify text channels of affected players
+		// will do once i am done with basic structs and stuff
+	},
+});
